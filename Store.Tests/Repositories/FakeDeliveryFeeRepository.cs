03b5c5b5034447f496b3cd35ffc5f9745b1c86ad
Igor Cerutti <igor.cpv2@gmail.com>
2023-08-14 19:22:32 -0300

    Adicionado queries e seus testes

---
+++ Store.Tests/Repositories/FakeDeliveryFeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Store.Domain.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Tests.Repositories
+{
+    public class FakeDeliveryFeeRepository : IdeliveryFeeRepository
+    {
+        public decimal Get(string zipCode)
+        {
+            if (zipCode == "123456")
+                return 10;
+            else if (zipCode == "122456")
+                return 7;
+
+            return 0;
+        }
+    }
+}
